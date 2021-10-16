94d14449568bf3ee98d8739d0dfe06a48be1ba61
seanlucano <seanlucano@gmail.com>
2021-10-16 14:44:12 -0700

    Step 4 text revision

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -1472,8 +1472,7 @@ var app = (function () {
             
         },
         {	title: 'Residuals',
-            prompt:` <p>Each of these points tell us about an <strong>observed</strong> transaction.  Our line, on the other hand, is attempting to predict future dollar vlues for any number of donuts purchased.</p>
-        <p>Sometimes the line makes a prediction about a transaction cost that we already know.</p>
+            prompt:` <p>Each points tells us about an <strong>observed</strong> transaction.  Our line, on the other hand, is making a <strong>prediction</strong> about a future transaction.</p>
         <p>The distance between the <strong>observed</strong> cost and the line's <strong>predicted</strong> cost is the <strong>residual</strong> (or error) value for the line at each point.</p>	
         `,
             cta: `<p><strong>Click on any point to see an explanation of the the residual cost for each transaction.</strong></p><p>You can view the explanation for either line, or for both at the same time<p>`,
